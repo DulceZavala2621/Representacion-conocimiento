00821bea010419e30f3072898c91decab630e6d1
DulceZavala2621 <74272706+DulceZavala2621@users.noreply.github.com>
2021-03-23 20:40:35 -0600

    Add files via upload

---
+++ Representación del conocimiento.pl
@@ -0,0 +1,88 @@
+medicina_natural (sabila).
+medicina_natural (aguacate).
+medicina_natural (manzanilla).
+medicina_natural (ajo).
+medicina_natural (árnica).
+medicina_natural (canela).
+medicina_natural (comino).
+medicina_natural (dienteLeon).
+
+medicina_quimica (aspirina).
+medicina_quimica (morfina).
+medicina_quimica (hormonas).
+medicina_quimica (talidomina).
+
+efectos_secundarios (X): - medicina_quimica (X).
+encuentra_en_naturaleza (X): - medicina_natural (X).
+
+depresión (aceitilla).
+
+cansansio (aceitilla).
+
+colicos (ajenjo).
+colicos (achicoria).
+colicos (anis).
+colicos (belladona).
+colicos (chicalote).
+colicos (cilantro).
+
+digestión (achicoria).
+digestión (cilantro).
+digestión (comino).
+
+afrodiciaco (aguacate).
+afrodiciaco (damiana).
+
+diabetes (aguacate).
+diabetes (alcachofa).
+diabetes (damiana).
+
+hemorroides (ahuehuete).
+
+anorexia (ajenjo).
+
+lombrices (ajo).
+lombrices (ajenjo).
+lombrices (amate).
+lombrices (brionia_blanca).
+lombrices (cempasúchil).
+
+caida_cabello (albahaca).
+
+higado (alcachofa).
+
+dolores (amapola_amarilla).
+dolores (árnica).
+
+tos (anacahuite).
+tos (bugambilia).
+tos (chicalote).
+
+bronquitis (anacahuite).
+
+asma (anacahuite).
+asma (belladona).
+asma (chicalote).
+asma (chile).
+
+tuberculosis (anacahuite).
+
+artitris (barcasco).
+
+reumatismo (amate).
+reumatismo (barcasco).
+reumatismo (chile).
+
+diurético (borraja).
+diurético (enebro).
+
+anemia (canela).
+anemia (cocolmeca).
+
+fertilidad (cocolmeca).
+
+retraso_menstrual (cocolmeca).
+
+insomnio (chicalote).
+
+estreñimiento (cuajiote).
